b0a97b74463641b7378cfd816a4202c1ea53d4c6
Fletcher Chapin <fletchapin@gmail.com>
2020-12-19 21:56:01 -0800

    Beat Day 13!!

--- 2020/challenge12.py
+++ 2020/challenge12.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import sys
 
 def lcm(a, b=None):
     if isinstance(a, (list, np.ndarray, tuple)):
@@ -32,68 +33,37 @@ def get_valid(possible, ids, diff):
 
     return result
 
-def process(ids, diff, lcm_list, sum=1, i=1, possible=[], limit=10000000):
+def process(ids, diff, lcm_list, sum=1, i=1, possible=[]):
     success = False
     while not success:
-        if i > limit:
-            break
         success = True
-        last = None
         for j, id in enumerate(ids):
-            if last is None:
-                if i % id == diff[j]:
-                    last = True
-                    continue
-                elif i % id == id - diff[j]:
-                    last = False
-                    continue
-                elif lcm_list[j] > sum:
-                    possible = process(ids, diff, lcm_list,
-                                       lcm_list[j], i=lcm_list[j] - i,
-                                       possible=possible)
-                    possible = process(ids, diff, lcm_list,
-                                       lcm_list[j], i=lcm_list[j] + i,
-                                       possible=possible)
-
-                    if j < len(ids) - 1 and i > 2 * lcm_list[j]:
-                        return possible
-                    else:
-                        sum = lcm_list[j]
-
-                i += sum
-                success = False
-                break
-
-            elif (i % id == diff[j] and not last):
-                last = True
-                continue
-            elif (i % id == id - diff[j] and last):
-                last = False
-                continue
-            else:
+            if i % id != id - diff[j]:
                 if lcm_list[j] > sum:
-                    possible = process(ids, diff, lcm_list,
-                                       lcm_list[j], i=lcm_list[j] - i,
-                                       possible=possible)
+                    # possible = process(ids, diff, lcm_list,
+                    #                    lcm_list[j], i=lcm_list[j] - i,
+                    #                    possible=possible)
                     possible = process(ids, diff, lcm_list,
                                        lcm_list[j], i=lcm_list[j] + i,
                                        possible=possible)
+                    return possible
 
                     if j < len(ids) - 1 and i > 2 * lcm_list[j]:
                         return possible
                     else:
                         sum = lcm_list[j]
 
-                i += sum
                 success = False
+                i += sum
                 break
 
-        if success:
-            possible.append(i)
+    if success:
+        possible.append(i)
+        i += sum
 
     return possible
 
-def run(filepath, i=1, limit=10000000):
+def run(filepath, i=1):
     file = open(filepath, 'r+')
     lines = [line.strip() for line in file.readlines()]
 
@@ -106,6 +76,7 @@ def run(filepath, i=1, limit=10000000):
           * bus_ids[departure_times.index(min(departure_times))])
 
     required_diff = [i % int(id) for i, id in enumerate(lines[1].split(',')) if id != 'x']
+    required_diff = [bus_ids[i] if diff == 0 else diff for i, diff in enumerate(required_diff)]
 
     start = int(lines[0])
     bus_ids = [int(id) for id in lines[1].split(',') if id != 'x']
@@ -117,16 +88,14 @@ def run(filepath, i=1, limit=10000000):
     least_common_multiples = [lcm(sorted_ids[0:j]) for j in range(1, len(sorted_ids))]
     least_common_multiples.insert(0, 1)
 
-    possible = process(sorted_ids, sorted_diff, least_common_multiples, sum=1,
-                       i=i, limit=limit)
+    possible = process(sorted_ids, sorted_diff, least_common_multiples, sum=1, i=i)
     possible.sort()
-    print(possible)
 
     # Answer to Part 2
     print(get_valid(list(set(possible)), bus_ids, required_diff))
 
-run('challenge12_test0.txt')
-run('challenge12_test1.txt')
-run('challenge12_test2.txt')
-run('challenge12_test3.txt')
-run('challenge12.txt', limit=10000000000000000)
+run('data/challenge12_test0.txt')
+run('data/challenge12_test1.txt')
+run('data/challenge12_test2.txt')
+run('data/challenge12_test3.txt')
+run('data/challenge12.txt')
